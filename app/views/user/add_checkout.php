63074ac76b5fbd4303b925a4ade8ac2f6f623578
Bard-o <simon.londono@utp.edu.co>
2025-05-25 18:17:57 -0500

    añade navbar

--- app/views/user/add_checkout.php
+++ app/views/user/add_checkout.php
@@ -34,6 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['buscar'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
+<?php include '../layouts/navbar_in.php';?>
 <div class="container mt-5">
     <h3>Solicitar Préstamo de Equipos</h3>
     <form method="POST">
