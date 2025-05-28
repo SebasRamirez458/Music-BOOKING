37a7531757bc3da0c37db595f1f70e0991206e04
Sebastian <sebastian.ramirez5@utp.edu.co>
2025-05-27 19:31:53 -0500

    pool mode

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ $db   = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
 $port = getenv('DB_PORT') ?: '5432';
-
+$pool_mode = 'transaction';
 try {
     $conn = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
